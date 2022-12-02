1b7397a598c6e7e596c158da3527715d8cf3ebaf
AndreasH96 <andreashaggstrom96@gmail.com>
2022-12-02 17:16:04 +0100

    Day 2 alternative solution made

--- AoC_2022/day2/src/main.rs
+++ AoC_2022/day2/src/main.rs
@@ -1,59 +1,46 @@
 use std::fs;
-use std::collections::HashMap;
+
 const TEST_PATH :&'static str= "./src/test_input.txt";
 const REAL_PATH :&'static str= "./src/real_input.txt";
 
-
-fn part1(path:&str)-> i64{
-    let translate_map = HashMap::from([
-        ("A X",1 + 3 ),
-        ("B X",1 + 0),
-        ("C X",1 + 6) ,
-        ("A Y",2 + 6) ,
-        ("B Y",2 + 3) ,
-        ("C Y",2 + 0) ,
-        ("A Z",3 + 0) ,
-        ("B Z",3 + 6) ,
-        ("C Z",3 + 3) ,
-    ]);
+macro_rules! calc_round_score {
+    ($a:expr,$b:expr) => {
+        ($a-87) + (1-((($a-$b)%3)%2)) * (6  -  3 * (($a-$b)%3)/2)
+    };
+    
+}
+fn part1(path:&str) ->i32{
     let contents = fs::read_to_string(path)
     .expect("Should have been able to read the file");
-    let rows:Vec<&str> = contents.split("\n").collect();
-    
-    let mut score = 0;
-    for row in &rows {
-        score =  score + translate_map.get(row).unwrap();
-    }
-   return score;
+    // Turn "A Y\nB X... into [[1,2],[2,1]]..."
+    let rows: Vec<Vec<i32>> = contents.split("\n").map(|r| vec![r.chars().next().unwrap() as i32 , r.chars().last().unwrap() as i32 ]).collect();
+    let sum:i32 = rows.iter().map(|r| calc_round_score!(r[1],r[0])).sum();
+    return sum;
 
-}  
-
-fn part2(path:&str)-> i64{
-    let translate_map = HashMap::from([
-        ("A X",3 + 0),
-        ("B X",1 + 0),
-        ("C X",2 + 0) ,
-        ("A Y",1 + 3) ,
-        ("B Y",2 + 3) ,
-        ("C Y",3 + 3) ,
-        ("A Z",2 + 6) ,
-        ("B Z",3 + 6) ,
-        ("C Z",1 + 6) ,
-    ]);
+}
+fn part2(path:&str) ->i32{
     let contents = fs::read_to_string(path)
     .expect("Should have been able to read the file");
-    let rows:Vec<&str> = contents.split("\n").collect();
+    // Turn "A Y\nB X... into [[1,2],[2,1]]..."
+    let rows: Vec<Vec<i32>> = contents.split("\n").map(|r| vec![r.chars().next().unwrap() as i32 , r.chars().last().unwrap() as i32 ]).collect();
     
-    let mut score = 0;
-    for row in &rows {
-        score =  score + translate_map.get(row).unwrap();
-    }
-   return score;
+    // Modifiers depending on A,B,C and X, Y, Z
+    //    X  Y  Z
+    // A +2  0 +1
+    // B -1  0 +1
+    // C -1  0 +1
+    // use this to turn i.e "A X" into "A Z" since we need to play "Z(scissors) to loose" 
+    let modifier_matrix = vec![[2,0,1],[-1,0,1],[-1,0,-2]];
 
-}  
+    let sum:i32 = rows.iter().map(|r| calc_round_score!(modifier_matrix[(r[0]-65) as usize][(r[1]-88) as usize] + 23 +r[0],r[0])).sum();
+    return sum;
+
+}
+
+
 
 fn main() {
-    
+
     println!("PART 1");
     let test_result = part1(TEST_PATH);
     println!("Test: {}", test_result);
@@ -65,5 +52,4 @@ fn main() {
     println!("Test: {}", test_result);
     let real_result = part2(REAL_PATH);
     println!("Real: {}", real_result);
-    
 }
---
+++ AoC_2022/day2/src/mains.rs
@@ -0,0 +1,74 @@
+use std::fs;
+use std::collections::HashMap;
+const TEST_PATH :&'static str= "./src/test_input.txt";
+const REAL_PATH :&'static str= "./src/real_input.txt";
+
+
+fn part1(path:&str)-> i64{
+    let translate_map = HashMap::from([
+        ("A X",1 + 3 ),
+        ("B X",1 + 0),
+        ("C X",1 + 6) ,
+        ("A Y",2 + 6) ,
+        ("B Y",2 + 3) ,
+        ("C Y",2 + 0) ,
+        ("A Z",3 + 0) ,
+        ("B Z",3 + 6) ,
+        ("C Z",3 + 3) ,
+    ]);
+    let contents = fs::read_to_string(path)
+    .expect("Should have been able to read the file");
+    let rows:Vec<&str> = contents.split("\n").collect();
+    
+    let mut score = 0;
+    for row in &rows {
+        score =  score + translate_map.get(row).unwrap();
+    }
+   return score;
+
+}  
+
+fn part2(path:&str)-> i64{
+    let translate_map = HashMap::from([
+        ("A X",3 + 0),
+        ("B X",1 + 0),
+        ("C X",2 + 0) ,
+        ("A Y",1 + 3) ,
+        ("B Y",2 + 3) ,
+        ("C Y",3 + 3) ,
+        ("A Z",2 + 6) ,
+        ("B Z",3 + 6) ,
+        ("C Z",1 + 6) ,
+    ]);
+    let contents = fs::read_to_string(path)
+    .expect("Should have been able to read the file");
+    let rows:Vec<&str> = contents.split("\n").collect();
+    
+    let mut score = 0;
+    for row in &rows {
+        score =  score + translate_map.get(row).unwrap();
+    }
+   return score;
+
+}  
+
+
+
+fn main() {
+
+    
+    println!("PART 1");
+    let test_result = part1(TEST_PATH);
+    println!("Test: {}", test_result);
+    let real_result = part1(REAL_PATH);
+    println!("Real: {}", real_result);
+
+    println!("PART 2");
+    let test_result = part2(TEST_PATH);
+    println!("Test: {}", test_result);
+    let real_result = part2(REAL_PATH);
+    println!("Real: {}", real_result);
+    
+  
+    
+}
